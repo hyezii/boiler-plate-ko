590dc909d02aa2204be0431df304ab3d0c0fe22c
hyeji Ahn <hyeziya@gmail.com>
2022-01-05 01:35:44 +0900

    비밀번호 암호화

--- index.js
+++ index.js
@@ -27,14 +27,15 @@ mongoose.connect(config.mongoURI)
 app.get('/', (req, res) => { res.send('Hello World! ') })
 
 
-// 회원가입을 위한 route
+// 회원가입을 위한 route   register route
 app.post('/register', (req, res) => {
 
         // 회원가입 할 때 필요한 정보들을 client에서 가져오면
         // 그것들을 데이터베이스에 넣어준다.
 
          const user = new User(req.body)  
-
+         
+         // 유저 정보 저장
          user.save((err, userInfo) => {
              if(err) return res.json({ success: false, err })
              return res.status(200).json({
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 // mongoose 모듈 가져옴
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+
 
 // Schema 생성
 const userSchema = mongoose.Schema({
@@ -32,6 +35,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+// user model에 유저 정보 저장하기 전에 하는 일
+userSchema.pre('save', function( next ){
+    var user = this;  // userSchema 가리킴
+
+    if(user.isModified('password')){  // 이메일, 이름 변경할 때 말고 비밀번호 변경할 때만
+
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {    // saltRounds를 사용해 salt 생성
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                // Store hash in your password DB.
+                next() // index.js의 유저 정보 저장으로 넘어감
+            })
+        })
+    }
+})
+    
 
 // schema를 model로 감싸줌
 const User = mongoose.model('User', userSchema)    
