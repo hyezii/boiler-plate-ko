dfd137fc6535537f32a924ddcddd8af8999854a0
hyeji Ahn <hyeziya@gmail.com>
2022-01-08 00:41:48 +0900

    Auth 기능

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 // body-Parser는 클라이언트에서 오는 정보를 서버에서 분석해 가져올 수 있게함
@@ -29,7 +29,7 @@ app.get('/', (req, res) => { res.send('Hello World! ') })
 
 
 // 회원가입을 위한 route   register route
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
         // 회원가입 할 때 필요한 정보들을 client에서 가져오면
         // 그것들을 데이터베이스에 넣어준다.
@@ -47,7 +47,7 @@ app.post('/register', (req, res) => {
 
 
 // login route
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     // 요청된 이메일이 데이터베이스에 있는지 찾음
     User.findOne({ email: req.body.email }, (err, user) =>{
@@ -77,5 +77,24 @@ app.post('/login', (req, res) => {
          })
      })
  })
+
+
+app.get('/api/users/auth', auth, (req, res) => {
+
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,       // role 0이면 일반유저, 아니면 관리자
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+
+
 
 app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) })
